ca21d4edc438ed227b797fc15f9d18d27f186212
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-11-08 11:43:32 -0800

    Initial

---
+++ src/BlazorBlogs/Program.cs
@@ -0,0 +1,20 @@
+// ============================================
+// Copyright (c) 2023. All rights reserved.
+// File Name :     Program.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : mpaulosky_App3
+// Project Name :  BlazorBlogs
+// =============================================
+
+using BlazorBlogs.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
